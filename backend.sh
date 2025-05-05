905d912fa3980cbeaf41bd07146fb3fb40940ceb
vamsi <vamsibattu3015@gmail.com>
2025-05-05 11:38:12 +0530

    backend6

--- backend.sh
+++ backend.sh
@@ -32,19 +32,19 @@ validate(){
 
 echo "script started executing at $( date )" | tee -a $log_file
 
-dnf module disable nodejs -y &>> log_file
+dnf module disable nodejs -y &>>log_file
 validate $? "disable default nodejs" 
 
-dnf module enable nodejs:20 -y &>> log_file
+dnf module enable nodejs:20 -y &>>log_file
 validate $? "enable nodejs:20"
 
-dnf install nodejs -y &>> log_file
+dnf install nodejs -y &>>log_file
 validate $? "install nodejs"
 
-id expense &>> log_file
+id expense &>>log_file
 if [ $? -ne 0 ]; then
     echo "expense user doesn't exist...creating"
-    useradd expense &>> log_file
+    useradd expense &>>log_file
     validate $? "creating expense user"
 else
     echo "user already exist"
@@ -53,12 +53,12 @@ fi
 mkdir -p /app
 validate $? "creating app folder"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>> log_file
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>log_file
 validate $? "downloading the code to tmp directory"
 
 cd /app
 rm -rf /app/* # remove the existing code
-unzip /tmp/backend.zip &>> log_file
+unzip /tmp/backend.zip &>>log_file
 validate $? "extracting backend application code"
 
 
